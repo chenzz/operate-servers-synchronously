068de764c70540fe0d3e4926cd76008bee85d5bf
chenzz <czzshr@gmail.com>
2017-12-04 20:45:40 +0800

    first version

---
+++ README.md
@@ -0,0 +1,10 @@
+# multi-servers-synchronize
+
+A tool used to ssh multi server and operate synchronizely
+
+
+
+### Reference
+NeteaseSshScript
+[TMUX – The Terminal Multiplexer (Part 2)](http://blog.hawkhost.com/2010/07/02/tmux-%E2%80%93-the-terminal-multiplexer-part-2/#tmux-shell-scripting)
+[a script to ssh multiple servers over multiple tmux panes](https://gist.github.com/johnko/a8481db6a83ec5ea2f37)
---
+++ sos.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+printUsage() 
+{
+	echo "Usage:$0 [-u user] [-p port] [-i identify_key] [-l ip_list_file] [ip1 ip2 ... ipn]"
+        echo "eg:$0 ip_list_file"
+}
+
+#parse argument
+has_specify_ip_list=0
+
+while getopts "hi:l:p:a:" option
+do
+	case $option in
+		i)
+			key="-i $OPTARG";;
+    		p)
+			port=$OPTARG;;
+    		u)
+			user=$OPTARG;;
+    		l)
+			ip_list_file=$OPTARG
+			has_specify_ip_list=1
+			;;
+		[?])
+			printUsage
+			exit 2
+			;;
+	esac
+done
+
+hosts=$*
+
+if [ 1 -eq ${has_specify_ip_list} ]
+then
+	hosts=`cat ${ip_list_file}`
+fi
+
+if [ -z "${hosts}" ]
+then
+	printUsage
+fi
+
+
+#construct tumux window
+tmux new-session -d -s localhost
+ 
+for host in ${hosts}
+do
+	tmux split-window -h "ssh ${host}"
+done
+
+tmux kill-pane -t 0
+tmux select-pane -t 0
+tmux select-layout tiled
+tmux set-window-option synchronize-panes on
+ 
+tmux -2 attach-session -t localhost
+
+
+
+
+
+
+
+
+
